19362344360efcd3080a00587ef7d5684d4fd5f8
Miguel Castillo <miguelct89@outlook.com>
2025-03-17 02:01:19 -0600

    Array y función de agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo () {
+    let amigo = document.getElementById('amigo');
+    if (amigo == '') {
+        alert('Inserta nombre de amigo válido.');
+    }
+}
